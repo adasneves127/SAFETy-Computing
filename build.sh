c0889fd8cadc9ff77ea07a14d4f1f76a7b755a67
Alex Dasneves <adasneves127@gmail.com>
2022-05-22 22:25:31 -0400

    Replaced build.sh with Makefile

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-clang++ -o Sim ./Software/Simulator/Main.cpp ./Software/Simulator/Memory.cpp ./Software/Simulator/ALU.cpp ./Software/Simulator/Register.cpp ./Software/Simulator/Registers.cpp
