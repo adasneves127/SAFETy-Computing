d4541cfd4e822db50d2981697fa5025c8834f852
Alex Dasneves <adasneves127@gmail.com>
2022-06-06 11:14:11 -0400

    Interim

--- Software/Assembler/AssemblerV2.py
+++ Software/Assembler/AssemblerV2.py
@@ -2,8 +2,15 @@ import assemblyDef   #This is the file that contains the assembly instructions
 import sys           #This is the file that contains the command line arguments
 import re
 
+# This is the name of the file that we will be reading.
 fileName = ""
+
+#Lines from the .aspect file will be stored here.
 inputLines = []
+
+# All "header" files will be included here.
+hPath = "./Asph/"
+
 
 #Output Lines:
 # [Address, Operation, Arguments, Machine Code]
@@ -17,6 +24,35 @@ labelsToResolve = []
 #   A dictionary of all reserverd labels to their location in memory
 #   A dictionary of all reserved variables to their location in memory
 
+#Convert between bases. This is used to convert numbers into hex.
+def convertBases(num: str, z: int):
+    if "0x" in num:
+        return ((z - len(num[2:])) * "0") + num[2:]
+    elif "0b" in num:
+        return ((z - len(hex(int(num, 2))[2:])) * "0") + hex(int(num, 2))[2:]
+    elif "0o" in num:
+        return ((z - len(hex(int(num, 8))[2:])) * "0") + hex(int(num, 8))[2:]
+    else:
+        return ((z - len(hex(int(num))[2:])) * "0") + hex(int(num))[2::]
+
+def resolveDirective(line: str):
+    global outputLines, outputIndex
+    global inputLines
+    splitLine = line.split(" ")
+    if(splitLine[0] == ".org"):
+        outputIndex = int("0x" + splitLine[1], 16)
+        return
+    if(splitLine[0] == ".inc"):
+        #Include file. File must be located within Asph folder.
+        try:
+            with open(hPath + splitLine[1]) as fileIn:
+                for line in fileIn:
+                    inputLines.append(line.lower())
+        except:
+            print("Error: File not found: " + splitLine[1])
+            print("Input line: " + inputLines.index(line)) #Print the line number that caused the error
+            sys.exit(1)
+
 def writeFiles():
     pass
 
@@ -24,7 +60,7 @@ def assemble():
     global inputLines, outputLines, outputIndex, linesToResolve, labelsToResolve
     for ind, data in enumerate(inputLines):
         if("." in data):
-            pass #This will be a directive. We will need to fix it later.
+            resolveDirective(data)
             continue
         if(":" in data):
             labelsToResolve.append([outputIndex, data])
@@ -32,15 +68,23 @@ def assemble():
         splitLine = data.split(" ")
         instruction = splitLine[0]
         isJump = re.match("(jmp|beq|bne|jsn|jse|jsr)", data)
-        if type(isJump) != None: #If it is a jump instruction, or it's derivates
+        if type(isJump) == None: #If it is a jump instruction, or it's derivates
             linesToResolve.append([outputIndex, instruction, splitLine[1::]])
             outputIndex += 3 #We will need to add 3 to the index, as all jump instructions are 3 bytes long. We will come back to it later.
             continue
+        print(f"[{ind}]: {data}")
         try:
-            outputLines[outputIndex] = assemblyDef.assemblyDict[instruction][0]
-            outputIndex += assemblyDef.assemblyDict[instruction][1]
+            if("$" in splitLine[-1]):
+                instruction += " $"
+            elif "#" in splitLine[-1]:
+                instruction += " #"
+            outputLines[outputIndex] = assemblyDef.opCodes[instruction][0]
+            if(assemblyDef.opCodes[instruction][1] > 0):
+                #If the instruction has arguments, we need to convert them to machine code.
+                pass
         except KeyError:
             print("Error: Invalid instruction: " + instruction)
+            print(f"Input line: {ind}, instruction: {data}") #Print the line number that caused the error
             sys.exit(1)
         
     writeFiles()
@@ -50,7 +94,7 @@ def readIn():
     #Open the file
     with open(fileName) as fileIn:
         for line in fileIn:
-            inputLines.append(line.lower())
+            inputLines.append(line.lower().strip())
     assemble()
 
 def getStartData():
